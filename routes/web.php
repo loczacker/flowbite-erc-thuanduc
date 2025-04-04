1884eddf54be5d04ba1684539e4415686c96163e
loczacker <tienloctao@gmail.com>
2025-04-04 15:29:32 +0700

    Routes app

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('corporations', fn () => Inertia::render('Organization/Corporations'))->name('organizations.corporations');
+    Route::get('companies', fn () => Inertia::render('Organization/Companies'))->name('organizations.companies');
+    Route::get('factories', fn () => Inertia::render('Organization/Factories'))->name('organizations.factories');
+    Route::get('departments', fn () => Inertia::render('Organization/Departments'))->name('organizations.departments');
 });
 
 require __DIR__.'/settings.php';
